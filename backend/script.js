ca82d58a953c995df4f719f01ae3a8754701fb37
AnkuThakur704 <ankushanku704@gmail.com>
2026-01-03 18:33:52 +0530

    Fix start script and port hndling

--- backend/script.js
+++ backend/script.js
@@ -1,9 +1,10 @@
 import express from "express"
 import router from "./routes/code.js"
 import cors from "cors"
+const port = process.env.PORT || 8080
 const app = express()
 app.use(cors())
 app.use("/api/routes",router)
-app.listen(8080,()=>{
-    console.log("running on 8080")
+app.listen(port,()=>{
+    console.log(`running on port:${port}`)
 })
